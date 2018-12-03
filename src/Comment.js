757400dd7455617e4c45a298e2d259092ed5912e
hyyqc <junjie_hyyqc@163.com>
2018-12-03 16:07:17 +0800

    优化代码, 隔段时间刷新

--- src/Comment.js
+++ src/Comment.js
@@ -13,12 +13,16 @@ export default class Comment extends Component{
 
     componentDidMount() {
         this._updateTimeString();
+        this._timer = setInterval(this._updateTimeString.bind(this), 5000) // 隔5秒刷新
+    }
+
+    componentWillUnmount() {
+        clearInterval(this._timer);
     }
 
     _updateTimeString() {
         const comment = this.props.comment;
         const duration = (+Date.now() - comment.createdTime) / 1000;
-        console.log(comment);
         this.setState({
             timeString: duration > 60 ?
                 `${Math.round(duration / 60)} 分钟前`
