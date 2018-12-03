a1e6b560df066b3de8acf68b40b4646f176c0adb
hyyqc <junjie_hyyqc@163.com>
2018-12-03 14:52:17 +0800

    优化代码, 增加类型检测

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,23 +1,58 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export default class CommentInput extends Component{
+    // 类型检测
+    static propTypes = {
+        onSubmit: PropTypes.func
+    };
+
     constructor(props) {
         super(props);
         this.state = {
             username: '',
             content: ''
-        }
+        };
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handleUsernameBlur = this.handleUsernameBlur.bind(this);
+        this.handleContentChange = this.handleContentChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    componentDidMount() {
+        this.text.focus(); // ref 自动聚焦
+        this._loadUsername();
+    }
+
     handleUsernameChange(e) {
         this.setState({
             username: e.target.value
         })
     }
-    handleConentChange(e) {
+
+    // 调用localStorage
+    _loadUsername() {
+        const username = localStorage.getItem('username');
+        if (username) {
+            this.setState({username})
+        }
+    }
+
+    // 用户名持久化 localStorage
+    _saveUsername(username) {
+        localStorage.setItem('username', username);
+    }
+
+    handleUsernameBlur(e) {
+        this._saveUsername(e.target.value);
+    }
+
+    handleContentChange(e) {
         this.setState({
             content: e.target.value
         })
     }
+
     handleSubmit() {
         if(this.props.onSubmit) {
             const {username, content} = this.state;
@@ -28,6 +63,7 @@ export default class CommentInput extends Component{
             content: ''
         })
     }
+
     render() {
         return (
             <div className="comment-input">
@@ -37,7 +73,8 @@ export default class CommentInput extends Component{
                         <input
                             type="text"
                             value={this.state.username}
-                            onChange={this.handleUsernameChange.bind(this)}
+                            onBlur={this.handleUsernameBlur}
+                            onChange={this.handleUsernameChange}
                         />
                     </div>
                 </div>
@@ -45,15 +82,14 @@ export default class CommentInput extends Component{
                     <span className="comment-field-name">评论内容: </span>
                     <div className="comment-field-input">
                         <textarea
-                            cols="30"
-                            rows="10"
+                            ref={(text) => this.text = text}
                             value={this.state.content}
-                            onChange={this.handleConentChange.bind(this)}
+                            onChange={this.handleContentChange}
                         />
                     </div>
                 </div>
                 <div className="comment-field-button">
-                    <button onClick={this.handleSubmit.bind(this)}>发布</button>
+                    <button onClick={this.handleSubmit}>发布</button>
                 </div>
             </div>
         )
